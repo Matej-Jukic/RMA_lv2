fc7d9ff42e171ff9e296f124b5173fffc815db3f
Matej-Jukic <matejjukiczuti@gmail.com>
2021-04-17 12:24:41 +0200

    Fixed URL-s

--- SmartQuotes/app/src/main/java/com/matejjukic/ferit/smartquotes/Persistence/PersonRepository.kt
+++ SmartQuotes/app/src/main/java/com/matejjukic/ferit/smartquotes/Persistence/PersonRepository.kt
@@ -9,10 +9,10 @@ object PersonRepository {
     }
     private fun retrievePersons(): MutableList<Person>{
         return mutableListOf(
-                Person(  "Dennis Ritchie", "1941", "2011", "UNIX is basically a simple OS but you have to be genius to understand simplicity", "https://hr.wikipedia.org/wiki/Dennis_Ritchie#/media/Datoteka:Dennis_Ritchie_2011.jpg"),
-                Person( "Bjarne Stroustrup", "1950", "", "There are only two kinds of languages: the ones people complain about and the ones nobody uses.", "https://bs.wikipedia.org/wiki/Bjarne_Stroustrup#/media/Datoteka:BjarneStroustrup.jpg"),
-                Person( "Steve Jobs", "1955", "2011", "I think the things you regret most in life are the things you didn’t do.", "https://hr.wikipedia.org/wiki/Steve_Jobs#/media/Datoteka:Steve_Jobs_Headshot_2010-CROP.jpg"),
-                Person( "Kale Gospodar Vremena", "Nepoznato", "Još ne", "Mralee", "https://www.facebook.com/1616925158571418/photos/a.1616929675237633/1616929681904299")
+                Person(  "Dennis Ritchie", "1941", "2011", "UNIX is basically a simple OS but you have to be genius to understand simplicity", "https://upload.wikimedia.org/wikipedia/commons/2/23/Dennis_Ritchie_2011.jpg"),
+                Person( "Bjarne Stroustrup", "1950", "", "There are only two kinds of languages: the ones people complain about and the ones nobody uses.", "https://upload.wikimedia.org/wikipedia/commons/d/da/BjarneStroustrup.jpg"),
+                Person( "Steve Jobs", "1955", "2011", "I think the things you regret most in life are the things you didn’t do.", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/1280px-Steve_Jobs_Headshot_2010-CROP.jpg"),
+                Person( "Kale Gospodar Vremena", "Nepoznato", "Još ne", "Mralee", "https://upload.wikimedia.org/wikipedia/commons/2/20/Boerenkool.jpg")
         )
     }
     fun remove(name: String) = persons.removeAll{ person -> person.name == name }
